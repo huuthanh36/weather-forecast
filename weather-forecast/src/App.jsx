af0e3c10319e2f92be75966d8f801f58eb26fd6f
Nguyễn Hữu Thành <121446657+huuthanh36@users.noreply.github.com>
2025-08-27 00:32:19 +0700

    update App

--- weather-forecast/src/App.jsx
+++ weather-forecast/src/App.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import { useState } from "react";
+import WeatherForecastBackground from "./components/WeatherForecastBackground";
 
-export default function App() {
+const App = () => {
+  const [weather] = useState(null);
+
+  // Hàm kiểm tra và trả về điều kiện thời tiết nếu có dữ liệu
+  const getWeatherCondition = () => {
+    if (!weather || !weather.weather || !weather.sys) return null;
+    return {
+      main: weather.weather[0].main,
+      isDay:
+        Date.now() / 1000 > weather.sys.sunrise &&
+        Date.now() / 1000 < weather.sys.sunset,
+    };
+  };
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen">
+      <WeatherForecastBackground condition={getWeatherCondition()} />
+      <div className="flex items-center justify-center p-6 min-h-screen">
+        <div
+          className="bg-transparent backdrop-filter backdrop-blur-md
+        rounded-xl shadow-2xl p-8 max-w-md text-white w-full border 
+        border-white/30 relative z-10 text-center"
+        >
+          <h1 className="text-3xl font-extrabold text-center mb-6">
+            Weather Forecast App
+          </h1>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default App;
